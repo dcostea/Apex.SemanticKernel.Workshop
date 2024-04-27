a198bc4683b674443f6be34c5ade0c4de3fc2666
Daniel <daniel_costea@ymail.com>
2024-04-27 11:57:33 +0200

    Fixes for Dall-E and for GPT-Vision.

--- Secrets.cs
+++ Secrets.cs
@@ -7,4 +7,8 @@ public class Secrets
     public const string ApyKey2 = "your key here";
     public const string Endpoint2 = "your endpoint here";
     public const string DeploymentName2 = "your deployment name here";
+
+    public const string ApyKey3 = "your key here";
+    public const string Endpoint3 = "your endpoint here";
+    public const string DeploymentName3 = "your deployment name here";
 }
