a7894e6be26d5f1609dc3f02b330627396a2480e
Daniel <daniel_costea@ymail.com>
2024-06-21 22:10:47 +0200

    Cleanup.

--- Secrets.cs
+++ Secrets.cs
@@ -1,36 +1,20 @@
 
 public class Secrets
 {
-    public const string ApyKey = "f30ffaf4fb9e4d2e8474357d667495b1";
-    public const string Endpoint = "https://apexo2.openai.azure.com/";
-    public const string DeploymentName = "gpt-4o";
-    public const string Model = "gpt-4o";
+    public const string ApyKey = "your key here";
+    public const string Endpoint = "your deployment name here";
+    public const string DeploymentName = "your key here";
 
-    public const string DeploymentName2 = "dall-e-3";
-    public const string Model2 = "dall-e-3";
+    public const string ApyKey2 = "your key here";
+    public const string Endpoint2 = "your endpoint here";
+    public const string DeploymentName2 = "your deployment name here";
 
-	public const string BingSearchKey = "6fa944855041426e836e6aca3a9d134a";
+    public const string ApyKey3 = "your key here";
+    public const string Endpoint3 = "your endpoint here";
+    public const string DeploymentName3 = "your deployment name here";
 
-    //public const string OpenAIModel = "gpt-3.5-turbo";
-    public const string OpenAIModel = "gpt-4o";
+    public const string BingSearchKey = "your bing search key here";
 
-    public const string OpenAIApiKey = "sk-proj-C8Q9tngOVs7iNcn2UIUiT3BlbkFJPqqnU9kGvDVWG8Q4Qsds";
-
-
-//     public const string ApyKey = "your key here";
-//     public const string Endpoint = "your deployment name here";
-//     public const string DeploymentName = "your key here";
-
-//     public const string ApyKey2 = "your key here";
-//     public const string Endpoint2 = "your endpoint here";
-//     public const string DeploymentName2 = "your deployment name here";
-
-//     public const string ApyKey3 = "your key here";
-//     public const string Endpoint3 = "your endpoint here";
-//     public const string DeploymentName3 = "your deployment name here";
-
-//     public const string BingSearchKey = "your bing search key here";
-
-//    public const string OpenAIModel = "your model here";
-//    public const string OpenAIApiKey = "your key here";
+    public const string OpenAIModel = "your model here";
+    public const string OpenAIApiKey = "your key here";
 }
