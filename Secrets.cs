4326e68bb05dba6d9a0c897f41c6e187e98abfd7
Daniel <daniel_costea@ymail.com>
2024-06-23 22:05:16 +0200

    Folder reorganizing. Cleanup.

--- Secrets.cs
+++ Secrets.cs
@@ -1,20 +1,15 @@
-
 public class Secrets
 {
+    // for Azure (including images)
     public const string ApyKey = "your key here";
     public const string Endpoint = "your deployment name here";
     public const string DeploymentName = "your key here";
+    public const string Model = "your key here";
 
-    public const string ApyKey2 = "your key here";
-    public const string Endpoint2 = "your endpoint here";
-    public const string DeploymentName2 = "your deployment name here";
-
-    public const string ApyKey3 = "your key here";
-    public const string Endpoint3 = "your endpoint here";
-    public const string DeploymentName3 = "your deployment name here";
-
+    // for search
     public const string BingSearchKey = "your bing search key here";
 
+    // for OpenAI account
     public const string OpenAIModel = "your model here";
     public const string OpenAIApiKey = "your key here";
 }
